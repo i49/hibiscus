09667efa0cb69af7c06fb635a09321fff7636910
i49 <julyborn715@gmail.com>
2016-10-31 23:04:26 +0900

    Added javadoc generation.

--- hibiscus/src/main/java/com/github/i49/hibiscus/json/JsonNumberImpl.java
+++ hibiscus/src/main/java/com/github/i49/hibiscus/json/JsonNumberImpl.java
@@ -3,7 +3,7 @@ package com.github.i49.hibiscus.json;
 import javax.json.JsonNumber;
 import javax.json.JsonValue;
 
-public abstract class JsonNumberImpl implements JsonNumber {
+abstract class JsonNumberImpl implements JsonNumber {
 
 	@Override
 	public ValueType getValueType() {
--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd hibiscus
-mvn install
+mvn install -Dmaven.javadoc.skip=true
 cd ../hibiscus-examples
 mvn package
 java -jar target/hibiscus-examples-0.1.0.jar
