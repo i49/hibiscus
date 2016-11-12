7acccc33f08abd204a6026e6bb75d9427b3af6b5
i49 <julyborn715@gmail.com>
2016-11-12 11:02:10 +0900

    Added code to show finally constructed JSON structure.

--- hibiscus-examples/src/main/java/com/github/i49/hibiscus/examples/JsonLoader.java
+++ hibiscus-examples/src/main/java/com/github/i49/hibiscus/examples/JsonLoader.java
@@ -1,24 +1,43 @@
 package com.github.i49.hibiscus.examples;
 
+import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.json.Json;
+import javax.json.JsonStructure;
 import javax.json.JsonValue;
+import javax.json.JsonWriter;
+import javax.json.JsonWriterFactory;
+import javax.json.stream.JsonGenerator;
 
 import com.github.i49.hibiscus.schema.problems.Problem;
 import com.github.i49.hibiscus.validation.JsonValidator;
 import com.github.i49.hibiscus.validation.ValidationResult;
 
-public abstract class JsonLoader {
+/**
+ * Utility class in order to show results of validation of JSON.
+ */
+class JsonLoader {
+	
+	private static final JsonWriterFactory writerFactory;
+	
+	static {
+		Map<String, Object> config = new HashMap<>();
+		config.put(JsonGenerator.PRETTY_PRINTING, true);
+		writerFactory = Json.createWriterFactory(config);
+	}
 
 	/**
-	 * Loads JSON from specified resource.
-	 * @param name name of resource to load
-	 * @param validator validator for this JSON
-	 * @return JSON value loaded
+	 * Loads JSON content from specified resource.
+	 * @param name name of resource to load.
+	 * @param validator validator for this JSON content.
+	 * @return JSON value loaded.
 	 */
 	public static JsonValue load(String name, JsonValidator validator) {
 	
@@ -29,20 +48,49 @@ public abstract class JsonLoader {
 			long startTime = System.currentTimeMillis();
 			ValidationResult result = validator.validate(reader);
 			long endTime = System.currentTimeMillis();
-			if (result.hasProblems()) {
-				for (Problem p: result.getProblems()) {
-					System.out.println(p);
-				}
-			} else {
-				System.out.println("No problem found.");
-			}
 			double elapsed = (endTime - startTime) * 0.001;
+			printProblems(result);
 			System.out.println("Time elapsed: " + elapsed + " seconds");
-			System.out.println();
+			printValue(result);
 			return result.getValue();
 		} catch (IOException e) {
-			System.err.println(e);
+			System.err.println(e.toString());
 			return null;
+		}
+	}
+	
+	private static void printProblems(ValidationResult result) {
+		if (result.hasProblems()) {
+			for (Problem p: result.getProblems()) {
+				System.out.println(p.toString());
+			}
+		} else {
+			System.out.println("No problem found.");
+		}
+	}
+	
+	private static void printValue(ValidationResult result) {
+		System.out.println("JSON constructed:");
+		try (JsonWriter writer = writerFactory.createWriter(new StandardOutputStream())) {
+			writer.write((JsonStructure)result.getValue());
+		}
+		System.out.println();
+		System.out.println();
+	}
+	
+	private JsonLoader() {
+	}
+	
+	private static class StandardOutputStream extends FilterOutputStream {
+
+		public StandardOutputStream() {
+			super(System.out);
+		}
+
+		@Override
+		public void close() throws IOException {
+			super.flush();
+			// Do not close.
 		}
 	}
 }
