acefc226782af83e681c6af1c1a69d8b7dfcba89
i49 <julyborn715@gmail.com>
2017-01-01 13:34:01 +0900

    Fixed filename of example jar.

--- run-tests.sh
+++ run-tests.sh
@@ -4,4 +4,4 @@ cd hibiscus
 mvn install -Dmaven.javadoc.skip=true
 cd ../hibiscus-examples
 mvn package
-java -jar target/hibiscus-examples-0.1.0-SNAPSHOT.jar
+java -jar target/hibiscus-examples-0.1.0.jar
